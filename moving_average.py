dfab1e7a18bfd199b17dfa6fed2396106fe268f5
Jonas Barrskog <barrskog@kth.se>
2018-10-14 17:20:23 +0200

    Add exceptions for input and corresponding unit tests

--- moving_average.py
+++ moving_average.py
@@ -1,23 +1,40 @@
 """A collection of different types of moving averages."""
+import pandas as pd
 
 
-def simple_moving_average(data, window):
+def simple_moving_average(data_frame, data_column, window):
     """Take the Sliding Moving Average of the pandas dataframe."""
+    # Checking data_frame
+    if not isinstance(data_frame, pd.DataFrame):
+        raise TypeError("The data_frame has to be a pandas.DataFrame.")
 
-    if data.index.is_monotonic_increasing:
-        short_rolling = data.rolling(window=window).mean()
+    # Checking column
+    if not isinstance(data_column, (str)):
+        raise TypeError("The data_column must be a string.")
+    elif data_column not in data_frame.columns:
+        raise ValueError("The data_column does not exist in the data frame.")
+
+    # Checking window
+    if isinstance(window, (bool)) or not isinstance(window, (int)):
+        raise TypeError("The window must be an integer greater than zero.")
+    elif window < 1 or window > len(data_frame):
+        raise ValueError('The window={} has to be between 1 and len(df).'
+                         .format(window))
+
+    if data_frame.index.is_monotonic_increasing:
+        simple_rolling = data_frame[data_column].rolling(window=window).mean()
     else:
-        short_rolling = data.rolling(window=window).mean().shift(-(window-1))
+        simple_rolling = data_frame[data_column].rolling(
+            window=window).mean().shift(-(window-1))
 
-    #data['SMA_{}'.format(window)] = short_rolling
+    data_frame['SMA_{}'.format(window)] = simple_rolling
 
-    # return data
-    return short_rolling
+    return data_frame
 
 
 def exponential_moving_average():
     """Take the Exponential Moving Average of the pandas dataframe."""
-    pass
+    raise NotImplementedError
 
 
 if __name__ == '__main__':
--- tests/test_moving_average.py
+++ tests/test_moving_average.py
@@ -11,11 +11,11 @@ class TestMovingAverage(unittest.TestCase):
     """Testing the moving_average-module."""
 
     def setUp(self):
-        """Setting up general settings for the unit tests."""
+        """Set up general settings for the unit tests."""
         # Set a seed for the random-algorithm
         np.random.seed(seed=1337)
 
-    def test_simple_moving_average_3_units(self):
+    def test_algorithm(self):
         """Testing the sliding_moving_average-method."""
         days = pd.date_range('2018-01-10', '2018-01-19', freq='D')
 
@@ -35,14 +35,15 @@ class TestMovingAverage(unittest.TestCase):
                            80+1/3])
 
         data_frame_ans = pd.DataFrame(
-            {'days': days, 'col2': answer})
+            {'days': days, 'SMA_3': answer})
         data_frame_ans = data_frame_ans.set_index('days')
 
-        pd.testing.assert_frame_equal(
-            moving_average.simple_moving_average(data_frame, 3), data_frame_ans)
+        np.testing.assert_array_equal(
+            moving_average.simple_moving_average(data_frame, 'col2', 3)[
+                'SMA_3'].values, answer)
 
-    def test_simple_moving_average_3_units_reversed_date(self):
-        """Testing the sliding_moving_average-method."""
+    def test_algorithm_reversed_date(self):
+        """Testing the sliding_moving_average-method with reversed dates."""
         days = pd.date_range('2018-01-10', '2018-01-19', freq='D')[::-1]
         data = np.random.randint(1, high=100, size=len(days))
         data_frame = pd.DataFrame({'days': days, 'col2': data})
@@ -59,12 +60,97 @@ class TestMovingAverage(unittest.TestCase):
                            np.nan,
                            np.nan])
 
-        data_frame_ans = pd.DataFrame(
-            {'days': days, 'col2': answer})
-        data_frame_ans = data_frame_ans.set_index('days')
-        print(moving_average.simple_moving_average(data_frame, 3))
-        pd.testing.assert_frame_equal(
-            moving_average.simple_moving_average(data_frame, 3), data_frame_ans)
+        np.testing.assert_array_equal(
+            moving_average.simple_moving_average(data_frame, 'col2', 3)[
+                'SMA_3'].values, answer)
+
+    def test_name_of_new_column(self):
+        """Testing that the returned data has the correct header."""
+        days = pd.date_range('2018-01-10', '2018-01-19', freq='D')[::-1]
+        data = np.random.randint(1, high=100, size=len(days))
+        data_frame = pd.DataFrame({'days': days, 'col2': data})
+        data_frame = data_frame.set_index('days')
+
+        answer = ['col2']
+        for window in range(1, 9):
+            data_frame = moving_average.simple_moving_average(
+                data_frame, 'col2', window)
+
+            answer.append('SMA_{}'.format(window))
+
+        self.assertEqual(list(data_frame), answer)
+
+        # Testing exceptions
+        for tst in [True, [1, 2, 3], 2, 2.0]:
+            self.assertRaises(TypeError, moving_average.simple_moving_average,
+                              data_frame, tst, 3)
+        self.assertRaises(ValueError, moving_average.simple_moving_average,
+                          data_frame, "some sting", 3)
+
+    def test_window(self):
+        """Testing different kinds of windows."""
+        days = pd.date_range('2018-01-10', '2018-01-19', freq='D')[::-1]
+        data = np.random.randint(1, high=100, size=len(days))
+        data_frame = pd.DataFrame({'days': days, 'col2': data})
+        data_frame = data_frame.set_index('days')
+
+        # Different integers
+        self.assertRaises(ValueError, moving_average.simple_moving_average,
+                          data_frame, 'col2', len(data_frame)+1)
+        self.assertRaises(ValueError, moving_average.simple_moving_average,
+                          data_frame, 'col2', len(data_frame)+10000)
+        self.assertRaises(ValueError, moving_average.simple_moving_average,
+                          data_frame, 'col2', 0)
+        self.assertRaises(ValueError, moving_average.simple_moving_average,
+                          data_frame, 'col2', -1)
+        self.assertRaises(ValueError, moving_average.simple_moving_average,
+                          data_frame, 'col2', -10000)
+
+        # Float, string, bool, complex, list
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          data_frame, 'col2', 3.)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          data_frame, 'col2', "three")
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          data_frame, 'col2', True)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          data_frame, 'col2', 3+5j)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          data_frame, 'col2', [1, 2, 3])
+
+    def test_data_column(self):
+        """Testing different kinds of data_column."""
+        days = pd.date_range('2018-01-10', '2018-01-19', freq='D')[::-1]
+        data = np.random.randint(1, high=100, size=len(days))
+        data_frame = pd.DataFrame({'days': days, 'col2': data})
+        data_frame = data_frame.set_index('days')
+
+        # Float, string, bool, complex, list
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          data_frame, 3.)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          data_frame, 'col2', "three")
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          data_frame, 'col2', True)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          data_frame, 'col2', 3+5j)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          data_frame, 'col2', [1, 2, 3])
+
+    def test_data_frame(self):
+        """Testing different kinds of data_frame."""
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          np.arange(10), 'col2', 3)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          1, 'col2', 3)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          1.0, 'col2', 3)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          True, 'col2', 3)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          "some string", 'col2', 3)
+        self.assertRaises(TypeError, moving_average.simple_moving_average,
+                          3+5j, 'col2', 3)
 
 
 if __name__ == '__main__':
